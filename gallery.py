c8e08c79a959c1c898e7876e841165954678b40e
EAntes-9E <antes.enr@obmontessori.edu.ph>
2026-05-07 04:01:35 +0800

    Create gallery.py

---
+++ gallery.py
@@ -0,0 +1,22 @@
+from pyscript import document
+
+photos = [
+    {"name": "Teachers Day", "desc": "We luv sir Calpo!", "img": "copy of IMG_5349.JPG"}, 
+    {"name": "Christmas Party", "desc": "Omg green "img": "copy of IMG_5708.JPG"}, 
+    {"name": "Food Fair", "desc": "Coco to my jam!", "img": "DSC00175.JPG"}, 
+]
+
+gallery = document.getElementById("gallery")
+
+html = ""
+
+for p in photos:
+    html += f"""
+    <div class="card">
+        <img src="{p['img']}" alt="{p['name']}">
+        <h3>{p['name']}</h3>
+        <p>{p['desc']}</p>
+    </div>
+    """
+
+gallery.innerHTML = html
